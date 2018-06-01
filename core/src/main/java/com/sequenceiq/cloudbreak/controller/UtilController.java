fc6c1b48fcdd7416ada7d46a8646284c8ce9afb1
Richard Doktorics <rdoktorics@hortonworks.com>
2018-06-01 15:35:02 +0200

    RMP-11521 Implement cloudstorage support matrix on backend side

--- core/src/main/java/com/sequenceiq/cloudbreak/controller/UtilController.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/controller/UtilController.java
@@ -1,26 +1,20 @@
 package com.sequenceiq.cloudbreak.controller;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
-
-import javax.inject.Inject;
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
 import com.sequenceiq.cloudbreak.api.endpoint.v1.UtilEndpoint;
 import com.sequenceiq.cloudbreak.api.model.AmbariDatabaseDetailsJson;
 import com.sequenceiq.cloudbreak.api.model.AmbariDatabaseTestResult;
 import com.sequenceiq.cloudbreak.api.model.ExposedServiceResponse;
 import com.sequenceiq.cloudbreak.api.model.ParametersQueryRequest;
 import com.sequenceiq.cloudbreak.api.model.ParametersQueryResponse;
+import com.sequenceiq.cloudbreak.api.model.StructuredParameterQueriesResponse;
+import com.sequenceiq.cloudbreak.api.model.StructuredParameterQueryResponse;
+import com.sequenceiq.cloudbreak.api.model.StructuredParametersQueryRequest;
 import com.sequenceiq.cloudbreak.api.model.VersionCheckResult;
+import com.sequenceiq.cloudbreak.api.model.filesystem.CloudStorageSupportedResponse;
 import com.sequenceiq.cloudbreak.api.model.rds.RDSBuildRequest;
 import com.sequenceiq.cloudbreak.api.model.rds.RdsBuildResult;
 import com.sequenceiq.cloudbreak.api.model.stack.StackMatrix;
+import com.sequenceiq.cloudbreak.blueprint.filesystem.query.ConfigQueryEntry;
 import com.sequenceiq.cloudbreak.common.model.user.IdentityUser;
 import com.sequenceiq.cloudbreak.controller.exception.BadRequestException;
 import com.sequenceiq.cloudbreak.controller.validation.rds.RdsConnectionBuilder;
@@ -28,7 +22,22 @@ import com.sequenceiq.cloudbreak.converter.stack.cluster.ServiceEndpointCollecto
 import com.sequenceiq.cloudbreak.service.AuthenticatedUserService;
 import com.sequenceiq.cloudbreak.service.StackMatrixService;
 import com.sequenceiq.cloudbreak.service.blueprint.BlueprintService;
+import com.sequenceiq.cloudbreak.service.filesystem.FileSystemSupportMatrixService;
 import com.sequenceiq.cloudbreak.util.ClientVersionUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @Component
 public class UtilController implements UtilEndpoint {
@@ -47,6 +56,13 @@ public class UtilController implements UtilEndpoint {
 
     @Autowired
     private AuthenticatedUserService authenticatedUserService;
+
+    @Autowired
+    private FileSystemSupportMatrixService fileSystemSupportMatrixService;
+
+    @Autowired
+    @Qualifier("conversionService")
+    private ConversionService conversionService;
 
     @Value("${info.app.version:}")
     private String cbVersion;
@@ -97,8 +113,38 @@ public class UtilController implements UtilEndpoint {
     }
 
     @Override
+    public Collection<CloudStorageSupportedResponse> getCloudStorageMatrix(String stackVersion) {
+        return fileSystemSupportMatrixService.getCloudStorageMatrix(stackVersion);
+    }
+
+    @Override
     public ParametersQueryResponse getCustomParameters(ParametersQueryRequest parametersQueryRequest) {
         IdentityUser user = authenticatedUserService.getCbUser();
-        return blueprintService.queryCustomParameters(parametersQueryRequest.getBlueprintName(), user);
+        Set<String> strings = blueprintService.queryCustomParameters(parametersQueryRequest.getBlueprintName(), user);
+        Map<String, String> result = new HashMap<>();
+        for (String customParameter : strings) {
+            result.put(customParameter, "");
+        }
+        ParametersQueryResponse parametersQueryResponse = new ParametersQueryResponse();
+        parametersQueryResponse.setCustom(result);
+        return parametersQueryResponse;
+    }
+
+    @Override
+    public StructuredParameterQueriesResponse getFileSystemParameters(StructuredParametersQueryRequest structuredParametersQueryRequest) {
+        IdentityUser user = authenticatedUserService.getCbUser();
+        List<ConfigQueryEntry> entries = blueprintService.queryFileSystemParameters(
+                structuredParametersQueryRequest.getBlueprintName(),
+                structuredParametersQueryRequest.getClusterName(),
+                structuredParametersQueryRequest.getStorageName(),
+                structuredParametersQueryRequest.getFileSystemType(),
+                user);
+        List<StructuredParameterQueryResponse> result = new ArrayList<>();
+        for (ConfigQueryEntry configQueryEntry : entries) {
+            result.add(conversionService.convert(configQueryEntry, StructuredParameterQueryResponse.class));
+        }
+        StructuredParameterQueriesResponse parametersQueryResponse = new StructuredParameterQueriesResponse();
+        parametersQueryResponse.setEntries(result);
+        return parametersQueryResponse;
     }
 }
